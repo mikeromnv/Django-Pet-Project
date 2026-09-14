3549d676d722d19c79b7f704b65ffe7bf7a7e83b
Mike <romanosir@yandex.ru>
2026-09-14 22:03:19 +0300

    Initial commit

---
+++ workouts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from workouts.views import *
+
+urlpatterns = [
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path('', home_view, name='home'),
+    path('logout/', logout_view, name='logout'),
+]
---
+++ workouts/views.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from workouts.forms import RegisterForm
+
+
+# Create your views here.
+def home_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'workouts/home.html')
+
+    return redirect('login')
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+
+            login(request, user)
+
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(
+        request,
+        'workouts/register.html',
+        {'form': form}
+    )
+
+def login_view(request):
+    error = None
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user=authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        error = "Неверный логин или пароль"
+    return render(
+        request,
+        'workouts/login.html',
+        {'error': error}
+    )
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
